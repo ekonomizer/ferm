c311bae5985dfbb1dc6e7698db0a28a5eba68cb3
ekonomizer <ekonomizer@mail.ru>
2011-11-02 19:47:10 +0400

    delete all files. next step copy in this branch flex app

--- app/models/seed.rb
+++
@@ -1,6 +0,0 @@
-class Seed < ActiveRecord::Base
-	validates :objectName,      :presence => true
-	validates :x,    			:presence => true
-	validates :y,				:presence => true
-	validates :plantedTime,		:presence => true
-end
--- spec/models/test_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe 'test' do
-  it 'test2'
-end
